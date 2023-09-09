60d5824a719b74c1c13a1e21e173a81c4435305e
Mateus Cruz <profissionalmateuscruz@gmail.com>
2023-09-09 20:21:41 -0300

    first commit

---
+++ src/main/java/com/example/avanadedesafiotecnico/AvanadeDesafioTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.avanadedesafiotecnico;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeDesafioTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeDesafioTecnicoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/avanadedesafiotecnico/AvanadeDesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avanadedesafiotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeDesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
