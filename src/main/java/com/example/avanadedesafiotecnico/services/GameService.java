c430df2c5290be116824880b36c97d1660f292fa
Mateus Cruz <profissionalmateuscruz@gmail.com>
2023-09-15 03:31:21 -0300

    fix: damage error

--- src/main/java/com/example/avanadedesafiotecnico/services/GameService.java
+++ src/main/java/com/example/avanadedesafiotecnico/services/GameService.java
@@ -161,178 +161,187 @@ import static com.example.avanadedesafiotecnico.services.utils.RandomUtils.*;
             }
         }
 
-//        public ResponseEntity defense(Long id) {
-//            var turnClass = turnRepository.findById(id);
-//            var battle = turnClass.get().getBattle();
-//
-//            if (battle.getStatus() == 2) {
-//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A batalha ja foi finalizada!");
-//            }
-//
-//            Battle battleOptional = battle;
-//
-//            Turn turnOptional = turnClass.get();
-//
-//            Turn turn = new Turn();
-//
-//            int defenseDice = (int) (Math.random() * 12) + 1;
-//
-//            Long character;
-//            if (turnOptional.getWhoTurn() == 1) {
-//                character = battleOptional.getCharacterId();
-//            } else {
-//                character = battleOptional.getMonsterId();
-//            }
-//
-//            Long classId = characterRepository.findById(character).get().getClass_id();
-//            int characterDefense = classRepository.findById(classId).get().getDefense();
-//            int characterAgility = classRepository.findById(classId).get().getAgility();
-//            int atual_defence_number = defenseDice + characterDefense + characterAgility;
-//
-//            turn.setAtualDefenseNumber(atual_defence_number);
-//            turn.setTurnType(1);
-//
-//            int atual_attack_number = turn.getAtualAttackNumber();
-//            boolean whoWins = atual_attack_number > atual_defence_number;
-//            int whoTurn = turn.getWhoTurn();
-//
-//            if (!whoWins) {
-//                if (whoTurn == 1) {
-//                    turn.setWhoTurn(2);
-//                } else {
-//                    turn.setWhoTurn(1);
-//                }
-//                turn.setTurnType(3);
-//                turn.setAtualAttackNumber(0);
-//                turn.setAtualDefenseNumber(0);
-//                battleOptional.setTurn(battleOptional.getTurn() + 1);
-//                turn.setTurnNumber(battleOptional.getTurn());
-//                turn.setWhoDamage(turn.getWhoTurn());
-//                turn.setEnemyLife(turnOptional.getEnemyLife());
-//                turn.setCharacterLife(turnOptional.getCharacterLife());
-//                turn.setBattle(battleOptional);
-//
-//                battleRepository.save(battleOptional);
-//                turnRepository.save(turn);
-//
-//                var response = new BattleResponseDTO();
-//                response.setMessage("O " + whoTurn + " conseguiu defender o ataque, o dano nao sera calculado, hora do ataque!");
-//                response.setTurn(turn);
-//                return ResponseEntity.ok(response);
-//            }
-//
-//
-//            try {
-//                battleRepository.save(battleOptional);
-//                turnRepository.save(turn);
-//
-//                var response = new BattleResponseDTO();
-//                if (turn.getWhoTurn() == 1) {
-//                    response.setMessage("O dado de defesa ficou em: " + atual_defence_number + "perdendo para o ataque de: " + atual_attack_number + " agora e a vez do aliado calcular o dano!");
-//                } else {
-//                    response.setMessage("O dado de defesa ficou em: " + atual_defence_number + "perdendo para o ataque de: " + atual_attack_number + " agora e a vez do inimigo calcular o dano!");
-//                }
-//
-//                response.setTurn(turn);
-//                return ResponseEntity.ok(response);
-//            } catch (Exception e) {
-//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao atacar, tente novamente.");
-//            }
-//        }
-//
-//        public ResponseEntity calculateDamage(Long id) {
-//            var turnClass = turnRepository.findById(id);
-//            var battle = turnClass.get().getBattle();
-//
-//            if (battle.getStatus() == 2) {
-//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A batalha ja foi finalizada!");
-//            }
-//
-//            Battle battleOptional = battle;
-//
-//            Turn turnOptional = turnClass.get();
-//
-//            Turn turn = new Turn();
-//
-//
-//            Long characterId;
-//            if (turn.getWhoTurn() == 1) {
-//                characterId = battleOptional.getCharacterId();
-//            } else {
-//                characterId = battleOptional.getMonsterId();
-//            }
-//
-//            int characterStrength;
-//            int quantity_data;
-//            int data_faces;
-//
-//            var characterClass = characterRepository.findById(characterId).get().getClass_id();
-//            var characterClassOptional = classRepository.findById(characterClass);
-//            if (characterClassOptional.isEmpty()) {
-//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Classe do personagem não encontrada!");
-//            }
-//
-//            var characterStatus = characterClassOptional.get();
-//            characterStrength = characterStatus.getStrength();
-//            quantity_data = characterStatus.getQuantity_data();
-//            data_faces = characterStatus.getData_faces();
-//
-//
-//            int damage = 0;
-//            for (int i = 0; i < quantity_data; i++) {
-//                int dice = (int) (Math.random() * data_faces) + 1;
-//                damage += dice;
-//            }
-//            damage += characterStrength;
-//
-//            if (damage < 0) {
-//                damage = 0;
-//            }
-//
-//            int enemyLife = turn.getEnemyLife();
-//            int characterLife = turn.getCharacterLife();
-//
-//            if (turn.getWhoTurn() == 1) {
-//                enemyLife -= damage;
-//                if (enemyLife < 0) {
-//                    enemyLife = 0;
-//                }
-//                turn.setEnemyLife(enemyLife);
-//            } else {
-//                characterLife -= damage;
-//                if (characterLife < 0) {
-//                    enemyLife = 0;
-//                }
-//                turn.setCharacterLife(characterLife);
-//            }
-//
-//            turn.setTurnType(1);
-//            turn.setAtualAttackNumber(0);
-//            turn.setAtualDefenseNumber(0);
-//            battleOptional.setTurn(battleOptional.getTurn() + 1);
-//            turn.setTurnNumber(battleOptional.getTurn());
-//            turn.setWhoDamage(3);
-//
-//            try {
-//                var response = new BattleResponseDTO();
-//                if (enemyLife <= 0 || characterLife <= 0) {
-//                    battleOptional.setStatus(2);
-//                    battleRepository.save(battleOptional);
-//                    turnRepository.save(turn);
-//
-//                    response.setMessage("O dano calculado foi de: " + damage + " agora o aliado tem " + turn.getCharacterLife() + " de vida e o inimigo tem " + turn.getEnemyLife() + " de vida! A batalha foi finalizada!");
-//                    response.setTurn(turn);
-//                    return ResponseEntity.ok(response);
-//                }
-//                battleRepository.save(battleOptional);
-//                turnRepository.save(turn);
-//
-//                response.setMessage("O dano calculado foi de: " + damage + " agora o aliado tem " + turn.getCharacterLife() + " de vida e o inimigo tem " + turn.getEnemyLife() + " de vida!");
-//                response.setTurn(turn);
-//                return ResponseEntity.ok(response);
-//            } catch (Exception e) {
-//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao calcular o dano, tente novamente.");
-//            }
-//        }
+        public ResponseEntity defense(Long id) {
+            var battle = battleRepository.findById(id);
+
+            if (battle.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Batalha nao encontrada!");
+            }
+
+            var battleOptional = battle.get();
+            var turn = battleOptional.getTurn();
+
+            if (battleOptional.getStatus() == 2) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A batalha ja foi finalizada!");
+            }
+
+            if (turn.getTurnType() != 2) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nao e o round de defesa!");
+            }
+
+            int defenseDice = (int) (Math.random() * 12) + 1;
+
+            Long character;
+            if (turn.getWhoTurn() == 1) {
+                character = battleOptional.getCharacterId();
+            } else {
+                character = battleOptional.getMonsterId();
+            }
+
+
+            Long classId = characterRepository.findById(character).get().getClass_id();
+            int characterDefense = classRepository.findById(classId).get().getDefense();
+            int characterAgility = classRepository.findById(classId).get().getAgility();
+            int atual_defence_number = defenseDice + characterDefense + characterAgility;
+
+            turn.setAtualDefenseNumber(atual_defence_number);
+
+            int atual_attack_number = turn.getAtualAttackNumber();
+            boolean userWins = atual_attack_number > atual_defence_number;
+            int whoTurn = turn.getWhoTurn();
+
+            if (!userWins) {
+                if (whoTurn == 1) {
+                    turn.setWhoTurn(2);
+                } else {
+                    turn.setWhoTurn(1);
+                }
+                turn.setTurnType(1);
+                turn.setAtualAttackNumber(0);
+                turn.setAtualDefenseNumber(0);
+                battleOptional.setTurnNumber(battleOptional.getTurnNumber() + 1);
+                turn.setWhoDamage(turn.getWhoTurn());
+
+                battleOptional.setTurn(turn);
+
+                turnRepository.save(turn);
+                battleRepository.save(battleOptional);
+
+                var response = new BattleResponseDTO();
+                response.setMessage("O " + whoTurn + " conseguiu defender o ataque, o dano nao sera calculado,proximo turno, hora do ataque!");
+                response.setBattle(battleOptional);
+                return ResponseEntity.ok(response);
+            }
+
+            turn.setTurnType(3);
+            battleOptional.setTurn(turn);
+
+            try {
+                battleRepository.save(battleOptional);
+                turnRepository.save(turn);
+
+                var response = new BattleResponseDTO();
+                if (turn.getWhoTurn() == 1) {
+                    response.setMessage("O dado de defesa ficou em: " + atual_defence_number + "perdendo para o ataque de: " + atual_attack_number + " agora e a vez do aliado calcular o dano!");
+                } else {
+                    response.setMessage("O dado de defesa ficou em: " + atual_defence_number + "perdendo para o ataque de: " + atual_attack_number + " agora e a vez do inimigo calcular o dano!");
+                }
+
+                response.setBattle(battleOptional);
+                return ResponseEntity.ok(response);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao atacar, tente novamente.");
+            }
+        }
+
+        public ResponseEntity calculateDamage(Long id) {
+            var battle = battleRepository.findById(id);
+
+            if (battle.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Batalha nao encontrada!");
+            }
+
+            var battleOptional = battle.get();
+            var turn = battleOptional.getTurn();
+
+            if(battleOptional.getStatus() == 2){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A batalha ja foi finalizada!");
+            }
+
+            if (turn.getTurnType() != 3) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nao e o round de calculo de dano!");
+            }
+
+
+            Long characterId;
+            if (turn.getWhoTurn() == 1) {
+                characterId = battleOptional.getCharacterId();
+            } else {
+                characterId = battleOptional.getMonsterId();
+            }
+
+            int characterStrength;
+            int quantity_data;
+            int data_faces;
+
+            var characterClass = characterRepository.findById(characterId).get().getClass_id();
+            var characterClassOptional = classRepository.findById(characterClass);
+            if (characterClassOptional.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Classe do personagem não encontrada!");
+            }
+
+            var characterStatus = characterClassOptional.get();
+            characterStrength = characterStatus.getStrength();
+            quantity_data = characterStatus.getQuantity_data();
+            data_faces = characterStatus.getData_faces();
+
+
+            int damage = 0;
+            for (int i = 0; i < quantity_data; i++) {
+                int dice = (int) (Math.random() * data_faces) + 1;
+                damage += dice;
+            }
+            damage += characterStrength;
+
+            if (damage < 0) {
+                damage = 0;
+            }
+
+            int enemyLife = turn.getEnemyLife();
+            int characterLife = turn.getCharacterLife();
+
+            if (turn.getWhoDamage() == 1) {
+                enemyLife -= damage;
+                if (enemyLife < 0) {
+                    enemyLife = 0;
+                }
+                turn.setEnemyLife(enemyLife);
+            } else {
+                characterLife -= damage;
+                if (characterLife < 0) {
+                    enemyLife = 0;
+                }
+                turn.setCharacterLife(characterLife);
+            }
+
+            turn.setTurnType(1);
+            turn.setAtualAttackNumber(0);
+            turn.setAtualDefenseNumber(0);
+            battleOptional.setTurnNumber(battleOptional.getTurnNumber() + 1);
+            turn.setWhoDamage(3);
+            battleOptional.setTurn(turn);
+
+            try {
+                var response = new BattleResponseDTO();
+                if (enemyLife <= 0 || characterLife <= 0) {
+                    battleOptional.setStatus(2);
+
+                    battleRepository.save(battleOptional);
+                    turnRepository.save(turn);
+
+                    response.setMessage("O dano calculado foi de: " + damage + " agora o aliado tem " + turn.getCharacterLife() + " de vida e o inimigo tem " + turn.getEnemyLife() + " de vida! A batalha foi finalizada!");
+                    response.setBattle(battleOptional);
+                    return ResponseEntity.ok(response);
+                }
+                battleRepository.save(battleOptional);
+                turnRepository.save(turn);
+
+                response.setMessage("O dano calculado foi de: " + damage + " agora o aliado tem " + turn.getCharacterLife() + " de vida e o inimigo tem " + turn.getEnemyLife() + " de vida!Hora do ataque!");
+                response.setBattle(battleOptional);
+                return ResponseEntity.ok(response);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao calcular o dano, tente novamente.");
+            }
+        }
     };
 
